4b6c1135479d2db1851c6651843fabad836d4798
ingecar <jorge.cardonaor@gmail.com>
2024-09-09 07:54:51 -0500

    clase1 javascript

---
+++ clase1.js
@@ -0,0 +1,33 @@
+/**
+ * Esto es un comentario de varias lineas
+ * Variables y Constantes
+ * Se puede utilizar caracteres $ y _ al inicio de las declaraciones
+ * Las variables que se declaran con let y constantes con const
+ */
+
+/*variables*/
+let numero1, numero2, NUM1;
+const pi = 3.14156;
+let estado = true;
+let pais = "Colombia";
+
+numero1 = 2;
+numero1 = 0;
+numero2 = 10;
+
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+// Mensajes en pantalla
+console.log("Hola Javascript");
+console.log(numero1);
+console.log(suma);
+
+// Concatenar valores
+console.log("El primer numero es: " + numero1);
+console.log("El primer numero es:", numero1);
+console.log(`La suma de ${numero1} + ${numero2} es: ${suma}`);
+console.log(`La resta de ${numero1} - ${numero2} es: ${resta}`);
+console.log(
+  `La multiplicacion de ${numero1} * ${numero2} es: ${multiplicacion}`
+);
